98af9ad7652a63174c50e752326d7ac70c3d3f7b
Agence Web La Capsule <webjungle66@gmail.com>
2025-03-16 23:57:29 +0100

    ok

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
-        <main className="relative">{props.children}</main>
+        <main className="relative">fffffffffffffffffff{props.children}</main>
       </body>
     </html>
   )
